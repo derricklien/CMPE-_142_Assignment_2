b9e12dc6405f9c709eca7d76dae487b45dccbb72
Salvador <salvador.rodriguez01@sjsu.edu>
2019-04-30 23:46:20 -0700

    main.cpp updated
    Added BJF

--- main.cpp
+++ main.cpp
@@ -137,14 +137,146 @@ int main()
     ElapsedTimeVec.clear();
     ResponceTimeVec.clear();
     
-    /////////SJF////////////
-    cout << "SJF: " <<endl;
-    cout << "Original entry\n";
+     cout << "Original entry\n";
     for (int i = 0; i <numberOFjobs; i++)
     {
         cout << "JobID: " <<job_id[i]<< ", ArrTime: "<< arrival_time[i] << ", Duration: " <<duration[i]<<endl;
     }
     
+    cout << "BJF"<<endl;
+    for (int  i = 0; i<numberOFjobs; i++)
+    {
+        for (int j = 0; j <numberOFjobs; j++)
+        {
+            if (arrival_time[i] < arrival_time[j])
+            {
+                //                cout <<"Need to swap: "<< duration[i] << ">" << duration[j] <<endl;
+                temp = duration[j];
+                duration[j] = duration[i];
+                duration[i] = temp;
+                
+                temp2 = arrival_time[j];
+                arrival_time[j] = arrival_time[i];
+                arrival_time[i] = temp2;
+                
+                temp3 = job_id[j];
+                job_id[j] = job_id[i];
+                job_id[i] = temp3;
+            }
+            if (arrival_time[i] == arrival_time[j])
+            {
+                if (duration[i] > duration[j])
+                {
+                    //                    cout <<"Need to swap: "<< duration[i] << ">" << duration[j] <<endl;
+                    temp = duration[j];
+                    duration[j] = duration[i];
+                    duration[i] = temp;
+                    
+                    temp2 = arrival_time[j];
+                    arrival_time[j] = arrival_time[i];
+                    arrival_time[i] = temp2;
+                    
+                    temp3 = job_id[j];
+                    job_id[j] = job_id[i];
+                    job_id[i] = temp3;
+                }
+            }
+        }
+    }
+    TT = duration[0];
+    for (int i = 1; i < numberOFjobs-1; i++)
+    {
+        for (int j = 2; j < numberOFjobs; j++)
+        {
+            //            cout <<"THE TT is: "<< TT<<", i is: "<<i<<", j is: "<<j<<endl; //debug
+            if (TT > arrival_time[j]&& i != numberOFjobs -2)
+            {
+                //                cout << "HERE: "<< arrival_time[j]<<endl;
+                if (duration[i] < duration[j])
+                {
+                    //                    cout << "Swap: "<< job_id[i] << "w/ " <<job_id[j]<<endl; //debug
+                    temp = duration[j];
+                    duration[j] = duration[i];
+                    duration[i] = temp;
+                    
+                    temp = arrival_time[j];
+                    arrival_time[j] = arrival_time[i];
+                    arrival_time[i] = temp;
+                    
+                    temp = job_id[j];
+                    job_id[j] = job_id[i];
+                    job_id[i] = temp;
+                }
+            }
+            else if (i == numberOFjobs -2)
+            {
+                if (duration[i] < duration[j] && j > i&& TT > arrival_time[i+1])
+                {
+                    //                    cout << "Swap: "<< job_id[i] << "w/ " <<job_id[j]<<endl; //debug
+                    temp = duration[j];
+                    duration[j] = duration[i];
+                    duration[i] = temp;
+                    
+                    temp = arrival_time[j];
+                    arrival_time[j] = arrival_time[i];
+                    arrival_time[i] = temp;
+                    
+                    temp = job_id[j];
+                    job_id[j] = job_id[i];
+                    job_id[i] = temp;
+                }
+            }
+        }
+        TT = TT + duration[i];
+    }
+    cout << "After BJF entry\n";
+    for (int i = 0; i <numberOFjobs; i++)
+    {
+        //            cout << "JobID: " <<job_id[i]<< ", ArrTime: "<< arrival_time[i] << ", Duration: " <<duration[i]<<endl;
+    }
+    for (int i = 0; i <numberOFjobs; i++)
+    {
+        //                cout << "JobID: " <<job_id[i]<< ", ArrTime: "<< arrival_time[i] << ", Duration: " <<duration[i]<<endl;
+        if (i == 0)
+        {
+            StartTimeVec.push_back(arrival_time[0]);
+            finish = arrival_time[0] + duration[0];
+            FinishTimeVec.push_back(finish);
+            ElapsedTimeVec.push_back(duration[0]);
+            responce = StartTimeVec[0] - arrival_time[0];
+            ResponceTimeVec.push_back(responce);
+        }
+        else
+        {
+            if (arrival_time[i] > FinishTimeVec[i-1])
+            {
+                StartTimeVec.push_back(arrival_time[i]);
+                finish = StartTimeVec[i] + duration[i];
+                FinishTimeVec.push_back(finish);
+                Et = finish - arrival_time[i];
+                ElapsedTimeVec.push_back(Et);
+                responce = StartTimeVec[i] - arrival_time[i];
+                ResponceTimeVec.push_back(responce);
+            }
+            else
+            {
+                StartTimeVec.push_back(FinishTimeVec[i-1]);
+                finish = StartTimeVec[i] + duration[i];
+                FinishTimeVec.push_back(finish);
+                Et = finish - arrival_time[i];
+                ElapsedTimeVec.push_back(Et);
+                responce = StartTimeVec[i] - arrival_time[i];
+                ResponceTimeVec.push_back(responce);
+            }
+        }
+        cout << "JD: "<< job_id[i]<< " StartTime: " << StartTimeVec[i] << " FinishTime: " << FinishTimeVec[i]<< " ElapsedTime: " << ElapsedTimeVec[i] << " ResponceTime: " << ResponceTimeVec[i]<<endl;
+    }
+    StartTimeVec.clear();
+    FinishTimeVec.clear();
+    ElapsedTimeVec.clear();
+    ResponceTimeVec.clear();
+    
+    /////////SJF////////////
     for (int  i = 0; i<numberOFjobs; i++)
     {
         for (int j = 0; j <numberOFjobs; j++)
@@ -183,31 +315,31 @@ int main()
             }
         }
     }
-//    cout << "Result\n";
-//    for (int i = 0; i <numberOFjobs; i++)
-//    {
-//        cout << "JobID: " <<job_id[i]<< ", ArrTime: "<< arrival_time[i] << ", Duration: " <<duration[i]<<endl;
-//    }
+    //    cout << "Result\n";
+    //    for (int i = 0; i <numberOFjobs; i++)
+    //    {
+    //        cout << "JobID: " <<job_id[i]<< ", ArrTime: "<< arrival_time[i] << ", Duration: " <<duration[i]<<endl;
+    //    }
     TT = duration[0];
     for (int i =1; i < numberOFjobs-1; i++) // might want to remove the minus 1
     {
         for (int j = 2; j < numberOFjobs; j++)
         {
-//            cout <<"THE TT is: "<< TT<<", i is: "<<i<<", j is: "<<j<<endl; //debug
+            //            cout <<"THE TT is: "<< TT<<", i is: "<<i<<", j is: "<<j<<endl; //debug
             if(TT>arrival_time[j] && i != numberOFjobs -2)
             {
-//                cout<< TT << " > "<< arrival_time[j]<<endl;
+                //                cout<< TT << " > "<< arrival_time[j]<<endl;
                 if(duration[i] > duration[j] && j > i)
                 {
-//                    cout << "Swap: "<< job_id[i] << "w/ " <<job_id[j]<<endl; //debug
+                    //                    cout << "Swap: "<< job_id[i] << "w/ " <<job_id[j]<<endl; //debug
                     temp = duration[j];
                     duration[j] = duration[i];
                     duration[i] = temp;
-
+                    
                     temp = arrival_time[j];
                     arrival_time[j] = arrival_time[i];
                     arrival_time[i] = temp;
-
+                    
                     temp = job_id[j];
                     job_id[j] = job_id[i];
                     job_id[i] = temp;
@@ -215,18 +347,18 @@ int main()
             }
             if ( i == numberOFjobs - 2)
             {
-//                cout << "NEWWW TT is: "<< TT<<", i is: "<<i<<", j is: "<<j<<endl;
+                //                cout << "NEWWW TT is: "<< TT<<", i is: "<<i<<", j is: "<<j<<endl;
                 if(duration[i] > duration[j] && j > i&& TT > arrival_time[i+1])
                 {
-//                    cout << "NEW SWAP: "<< job_id[i] << "w/ " <<job_id[j]<<endl; //debug
+                    //                    cout << "NEW SWAP: "<< job_id[i] << "w/ " <<job_id[j]<<endl; //debug
                     temp = duration[j];
                     duration[j] = duration[i];
                     duration[i] = temp;
-
+                    
                     temp = arrival_time[j];
                     arrival_time[j] = arrival_time[i];
                     arrival_time[i] = temp;
-
+                    
                     temp = job_id[j];
                     job_id[j] = job_id[i];
                     job_id[i] = temp;
@@ -236,15 +368,11 @@ int main()
         TT = TT + duration[i];
     }
     
-  /*  
     cout << "AFTER SJF entry\n";
     for (int i = 0; i <numberOFjobs; i++)
     {
-        cout << "JobID: " <<job_id[i]<< ", ArrTime: "<< arrival_time[i] << ", Duration: " <<duration[i]<<endl;
+        //        cout << "JobID: " <<job_id[i]<< ", ArrTime: "<< arrival_time[i] << ", Duration: " <<duration[i]<<endl;
     }
-   */
-    
-    cout << "SJF: " << endl;
     TT = duration[0];
     for (int i = 0; i <numberOFjobs; i++)
     {
@@ -261,7 +389,6 @@ int main()
         {
             if (arrival_time[i] > FinishTimeVec[i-1])
             {
-//                cout << "YEE\n";
                 StartTimeVec.push_back(arrival_time[i]);
                 finish = StartTimeVec[i]+ duration[i];
                 FinishTimeVec.push_back(finish);
@@ -272,7 +399,6 @@ int main()
             }
             else
             {
-//                cout<<"NAH\n";
                 StartTimeVec.push_back(FinishTimeVec[i-1]);
                 finish = StartTimeVec[i] + duration[i];
                 FinishTimeVec.push_back(finish);
@@ -280,9 +406,136 @@ int main()
                 ElapsedTimeVec.push_back(Et);
                 responce = StartTimeVec[i] - arrival_time[i];
                 ResponceTimeVec.push_back(responce);
-            }   
+            }
+            
         }
-        cout << "JID: "<< job_id[i]<< " StartTime: " << StartTimeVec[i] << " FinishTime: " << FinishTimeVec[i]<< " ElapsedTime: " << ElapsedTimeVec[i] << " ResponseTime: " << ResponceTimeVec[i]<<endl;
+        cout << "JD: "<< job_id[i]<< " StartTime: " << StartTimeVec[i] << " FinishTime: " << FinishTimeVec[i]<< " ElapsedTime: " << ElapsedTimeVec[i] << " ResponceTime: " << ResponceTimeVec[i]<<endl;
+    }
+    int timeCount=FinishTimeVec[numberOFjobs-1];
+    vector <int> RemainingTime;
+
+    StartTimeVec.clear();
+    FinishTimeVec.clear();
+    ElapsedTimeVec.clear();
+    ResponceTimeVec.clear();
+    RemainingTime.clear();
+
+
+    for (int i =0; i <numberOFjobs; i++)
+    {
+        StartTimeVec.push_back(0);
+        FinishTimeVec.push_back(0);
+        ElapsedTimeVec.push_back(0);
+        ResponceTimeVec.push_back(0);
+        RemainingTime.push_back(-10);
+    }
+    StartTimeVec.clear();
+    FinishTimeVec.clear();
+    ElapsedTimeVec.clear();
+    ResponceTimeVec.clear();
+    RemainingTime.clear();
+
+//    cout << "STCF entry\n";
+    for (int  i = 0; i<numberOFjobs; i++)
+    {
+        for (int j = 0; j <numberOFjobs; j++)
+        {
+            if (arrival_time[i] < arrival_time[j]) // arrange 1st arrival to last arrival using bubble sort
+            {
+                //                cout <<"Need to swap: "<< duration[i] << ">" << duration[j] <<endl;
+                temp = duration[j];
+                duration[j] = duration[i];
+                duration[i] = temp;
+                
+                temp2 = arrival_time[j];
+                arrival_time[j] = arrival_time[i];
+                arrival_time[i] = temp2;
+                
+                temp3 = job_id[j];
+                job_id[j] = job_id[i];
+                job_id[i] = temp3;
+            }
+            else if (arrival_time[i] == arrival_time[j]) // if arrival time eqaul, arrange based on job duration
+            {
+                if (duration[i] < duration[j])
+                {
+                    temp = duration[j];
+                    duration[j] = duration[i];
+                    duration[i] = temp;
+                    
+                    temp2 = arrival_time[j];
+                    arrival_time[j] = arrival_time[i];
+                    arrival_time[i] = temp2;
+                    
+                    temp3 = job_id[j];
+                    job_id[j] = job_id[i];
+                    job_id[i] = temp3;
+                }
+            }
+        }
+    }
+//    cout << "order in shortest arrival time\n";
+    int remain = 0;
+    for (int i = 0; i <numberOFjobs; i++)
+    {
+//        cout << "JobID: " <<job_id[i]<< ", ArrTime: "<< arrival_time[i] << ", Duration: " <<duration[i]<<endl;
+    }
+    for (int i =1; i < numberOFjobs-1; i++)
+    {
+        for (int j =2; j < numberOFjobs; j++)
+        {
+            //            cout <<"THE TT is: "<< TT<<", i is: "<<i<<", j is: "<<j<<endl; //debug
+            if (duration[i] < duration[j])
+            {
+                //                cout <<"HERE\n";
+            }
+        }
+    }
+    for (int i =0; i <numberOFjobs; i++)
+    {
+        for (int j = 0; j < numberOFjobs; j++)
+        {
+//            cout << "i is: "<<i<<", j is: "<<j<<", RT: "<<RemainingTime[i]<<endl; //debug
+            if(i==0)
+            {
+                StartTimeVec[i]= arrival_time[i];
+                responce = StartTimeVec[i] - arrival_time[i];
+                ResponceTimeVec[i]= responce;
+                if(duration[i] < arrival_time[i+1])
+                {
+//                    cout << "ran\n";
+                    FinishTimeVec[i]=duration[i];
+                }
+                else
+                {
+                    remain = duration[i] - arrival_time[i+1];
+                    RemainingTime[i]=remain;
+                    FinishTimeVec[i]=RemainingTime[i];
+                    ElapsedTimeVec[i] =RemainingTime[i];
+                }
+            }
+            else if(RemainingTime[j] < duration[j] && RemainingTime[j]!=-10)
+            {
+//                cout << "R < D "<< RemainingTime[j]<< " < "<< duration[j]<<endl;
+//                cout <<"i: "<< i<<", j: "<< j<<endl;
+            }
+            else if(RemainingTime[j] > duration[j] )
+            {
+//                cout << "R > D "<< RemainingTime[j]<< " > "<< duration[j]<<endl;
+            }
+            else if (RemainingTime[j] == 50)
+            {
+//                cout << "HERERE\n";
+            }
+        }
+        
+    }
+    
+//    cout << "C: "<< timeCount<<endl;
+    cout << "STCF Result\n";
+    for (int i = 0; i <numberOFjobs; i++)
+    {
+        cout<< "JD: "<< job_id[i]<< " StartTime: " << StartTimeVec[i] << " FinishTime: " << FinishTimeVec[i]<< " ElapsedTime: " << ElapsedTimeVec[i] << " ResponceTime: " << ResponceTimeVec[i]<< " Remain: "<<RemainingTime[i]<<endl;
     }
     StartTimeVec.clear();
     FinishTimeVec.clear();
